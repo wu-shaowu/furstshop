2d3f6888c2ec4c21681aeea3c30001bc97ea75d5
wu-shaowu <109330418+wu-shaowu@users.noreply.github.com>
2022-08-17 12:58:50 +0800

    Update Header.vue

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -224,7 +224,7 @@ export default {
         }
 
         .typeListRight:hover {
-          color: #f40;
+          color: #f40 !important;
           display: block;
           transform: scale(1.1, 1.1);
           text-decoration: none;
